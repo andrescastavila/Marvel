91911f21e8f2290ff157ca784316d636bc30f3f8
Jordi <jordigarcia81@hotmail.com>
2022-04-28 15:43:28 +0200

    created folder common

---
+++ src/app/shared/models/common/common.ts
@@ -0,0 +1,14 @@
+export interface Url{
+    type : string;
+    url : string;
+}
+
+export interface SeriesSummary{
+    resourceURI:string;
+    name:string;
+}
+
+export interface Image{
+    path : string
+    extension : string
+}
