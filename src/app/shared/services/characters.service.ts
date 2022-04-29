48cf3f278771f31eddde4c66f21a65f465896bf5
Jordi <jordigarcia81@hotmail.com>
2022-04-29 14:47:17 +0200

    changes on characters services

--- src/app/shared/services/characters.service.ts
+++ src/app/shared/services/characters.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable , of, from} from 'rxjs';
 import * as CryptoJS from 'crypto-js';
 import { Character } from '../models/characters/Character';
+import { map, } from 'rxjs/operators';
 
 
 
@@ -12,13 +13,19 @@ import { Character } from '../models/characters/Character';
 export class CharactersService {
   private publicApiKey="e84f95db87a67cb86fa5ebb74f999f9b";
   private secretApiKey="cdfa8e36af81f1e5faa8983a69758cd5562c39a4";
- 
+  public characters : Character[] =[];
   constructor(public http: HttpClient) { }
 
   public getCharacters(): Observable<Character>{
   
+    
     const ts = Date.now();
-    const url=`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${this.publicApiKey}&hash=${CryptoJS.MD5(ts+this.secretApiKey+this.publicApiKey)}`;
-    return this.http.get(url);
+    const url=`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${this.publicApiKey}&hash=${CryptoJS.MD5(ts+this.secretApiKey+this.publicApiKey).toString()}`;
+    //this.http.get()
+    const result  = from(this.http.get<Character>(url));
+     console.log(result);
+     return result;
+      
+    
   }
 }
