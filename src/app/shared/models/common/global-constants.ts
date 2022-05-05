d15b2862c3e3bda81e62cc67248e488474f6fd12
fran <francisco.pastor@colba.es>
2022-05-05 11:01:03 +0200

    add data source

--- src/app/shared/models/common/global-constants.ts
+++ src/app/shared/models/common/global-constants.ts
@@ -1,4 +1,8 @@
 export class GlobalConstants {
 
     public static apiUrl : String = "https://gateway.marvel.com:443/v1/public/";
+
+    public static  publicApiKey: String="e84f95db87a67cb86fa5ebb74f999f9b";
+    public static  secretApiKey: String="cdfa8e36af81f1e5faa8983a69758cd5562c39a4";
+    public static ts = Date.now();
 }
