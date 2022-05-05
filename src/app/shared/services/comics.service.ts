4ef62e53d5e929e934528183bb4fc7289d753a56
Jordi <jordigarcia81@hotmail.com>
2022-05-05 12:05:15 +0200

    completed data-source

--- src/app/shared/services/comics.service.ts
+++ src/app/shared/services/comics.service.ts
@@ -2,24 +2,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable , of, from} from 'rxjs';
-import * as CryptoJS from 'crypto-js';
 import { map, } from 'rxjs/operators';
 import { GlobalConstants } from '../models/common/global-constants';
 import { Comic } from '../models/comics/comics';
+import { DataSource } from './datasource.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ComicsService {
+export class ComicsService extends DataSource {
   comicId = 82967;
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient) {
+    super();
+  }
 
   public getComics():Observable<Comic[]>{
 
-    const publicApiKey="e84f95db87a67cb86fa5ebb74f999f9b";
-    const secretApiKey="cdfa8e36af81f1e5faa8983a69758cd5562c39a4";
-    const ts = Date.now();
-    const url=`${GlobalConstants.apiUrl}comics?ts=${ts}&apikey=${publicApiKey}&hash=${CryptoJS.MD5(ts+secretApiKey+publicApiKey).toString()}`;
+    
+    const url=`${GlobalConstants.apiUrl}comics?${this.getUrlParams()}`;
 
     return this.http.get(url).pipe(
       map((res:any)=>{
@@ -30,10 +30,8 @@ export class ComicsService {
   }
 
     public getComicById(comicId : number):Observable<Comic[]>{
-    const publicApiKey="e84f95db87a67cb86fa5ebb74f999f9b";
-    const secretApiKey="cdfa8e36af81f1e5faa8983a69758cd5562c39a4";
-    const ts = Date.now();
-    const url=`${GlobalConstants.apiUrl}comics/${comicId}?ts=${ts}&apikey=${publicApiKey}&hash=${CryptoJS.MD5(ts+secretApiKey+publicApiKey).toString()}`;
+    
+      const url=`${GlobalConstants.apiUrl}comics/${comicId}?${this.getUrlParams()}`;
   
     return this.http.get(url).pipe(
       map((res:any)=>{
